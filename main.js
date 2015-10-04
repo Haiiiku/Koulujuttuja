de62884f14137fc8828f11ac820ef531f2bbd017
Nixxti <Oskari.karjalainen@hotmail.fi>
2015-10-04 20:13:30 +0300

    Final JS

--- main.js
+++ main.js
@@ -2,20 +2,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var title = document.getElementById("title");
   var desc = document.getElementById("desc");
-  var expire = document.getElementById("expire");
+  var color = document.getElementById("colortag");
 });
 
-notes = [];
 function Submit() {
-  //console.log(title.innerText);
-  notes.push(new NoteObject(title.value,desc.value,expire.value));
-  //thing.returnInfo();
-  /*notes[notes.length-1].addEventListener("onClick", function(event){
-    alert("hi");
-  });*/
-  CreateNote(notes[notes.length-1].returnInfo());
+  CreateNote([title.value,desc.value,colortag.value]);
 }
 
+// Funktio joka luo listauksen.
 function CreateNote(data) {
   var noteDisplay = document.createElement("li");
   var title = document.createElement("h1");
@@ -24,25 +18,18 @@ function CreateNote(data) {
   description.innerText = data[1];
   noteDisplay.appendChild(title);
   noteDisplay.appendChild(description);
-  noteDisplay.style.background="gray";
-  noteDisplay.style.color="white";
-  noteDisplay.style.width = "50px;";
-  noteDisplay.style.height = "50px;";
+  noteDisplay.style.background = data[2];
+  noteDisplay.style.opacity = 1;
+
+  //Listauksien "poisto" toiminto
+  noteDisplay.addEventListener("click", function(event){
+    var interval = setInterval(function(){
+      noteDisplay.style.opacity -= 0.15;
+      if (noteDisplay.style.opacity <= 0) {
+        noteDisplay.style.display = "none";
+        clearInterval(interval);
+      }
+    }, 50);
+  });
   document.getElementById("nc").insertBefore(noteDisplay, document.getElementById("nc").childNodes[0]);
-  console.log("Tried to create div");
-}
-
-
-// Objekti to-do listauksille
-function NoteObject(noteTitle,noteDescription,noteExpire) {
-  this.nTitle = noteTitle;
-  this.nDesc = noteDescription;
-  this.nExpire = noteExpire;
-
-  this.returnInfo = ReturnInfo;
-}
-
-function ReturnInfo() {
-  var info = [this.nTitle,this.nDesc,this.nExpire];
-  return info;
 }
